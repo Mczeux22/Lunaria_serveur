417866c8e406a7673504e028ae6a007b770d483c
ubuntu <paponloic@outlook.fr>
2026-01-27 13:32:33 +0000

    modif header ajout des blibliotheque ubuntu

--- Include/header.h
+++ Include/header.h
@@ -2,8 +2,46 @@
  * @ Author: Lopapon
  * @ Create Time: 2026-01-26 20:43:12
  * @ Modified by: Lopapon
- * @ Modified time: 2026-01-26 21:05:10
+ * @ Modified time: 2026-01-27 13:30:12
  * @ Description:
  */
 
- 
+#ifndef HEADER_H
+#define HEADER_H
+
+/* Standard Libraries */
+
+#include <iostream> // std::cout, std::cerr
+#include <string> // std::string
+#include <vector> // std::vector
+#include <map> // std::map
+#include <set> // std::set
+#include <list> // std::list
+#include <memory> // std::shared_ptr, std::unique_ptr
+#include <algorithm> // std::find, std::sort, std::for_each
+#include <cstdlib> // std::rand, std::srand, std::atoi
+#include <ctime> // std::time, std::difftime
+#include <fstream> // std::ifstream, std::ofstream, std::fstream
+
+/* Asio Library for networking */
+
+#include <asio.hpp> // Asio Library
+#include <functional> //Callbacks
+#include <chrono> //Time management
+
+/* Multithreading support */
+
+#include <thread> // std::thread
+#include <mutex> // std::mutex, lock_guard, unique_lock
+#include <atomic> // std::atomic
+#include <condition_variable>
+
+/* Error & security */
+#include <stdexcept> // std::runtime_error, logic_error
+#include <cassert> // assert()
+#include <exception> // std::exception
+
+/*Intern Functionality*/
+
+
+#endif // HEADER_H
--- Serveur/main_server.cpp
+++ Serveur/main_server.cpp
@@ -1,3 +1,10 @@
+/**
+ * @ Author: Lopapon
+ * @ Create Time: 2026-01-26 19:32:20
+ * @ Modified by: Lopapon
+ * @ Modified time: 2026-01-27 12:54:33
+ * @ Description:
+ */
 
 
 int	main(void)
