72a50144a4ad491f7489addf06c6c3e0d35c9d3c
ubuntu <paponloic@outlook.fr>
2026-02-23 23:03:36 +0000

    libft suppr

--- libft/libft.h
+++
@@ -1,9 +0,0 @@
-/**
- * @ Author: Lopapon
- * @ Create Time: 2026-01-27 13:34:33
- * @ Modified by: Lopapon
- * @ Modified time: 2026-01-27 18:26:40
- * @ Description:
- */
-
-#include "../Include/header.h"
